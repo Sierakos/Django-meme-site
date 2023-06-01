deeba707c4448fc3524cb4d42452c2a4587ac26d
Sebastian Sieradzki <90157653+Sierakos@users.noreply.github.com>
2023-06-01 15:54:38 +0200

    Delete Selenium tests/tests directory

--- Selenium tests/tests/test_account.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-import time
-
-PATH = "C:\Program Files (86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-driver.get("http://127.0.0.1:8000/")
-time.sleep(5)
